a604b3ec99c189708dd85da50247d3184baa3f0b
?? <gynjy10@gmail.com>
2025-03-21 17:00:23 +0900

    mathapp방향, db개념이미지추가

---
+++ MathApp/B_data/studyCategory.js
@@ -0,0 +1,26 @@
+const studyCategory = {
+    "학습실메인(contents)": {
+      "c-principle": "개념설명 (정의와 원리의 이해)", 
+      "c-basic": "기본문제 (이해력 연습)", 
+      "c-training": "기본과제 (이해력 연향과제 - 새로운 문제구성)", 
+      "c-intensive": "심화과제 (문제해결력 확장과제 - 난이도문제구성)", 
+      "c-check": "과제확인 (이해력문제해결력 확인 - 과제AB)"
+    },
+    "평가&확인&수능(exercise)": {
+      "e-test": "평가A (이해력 확인평가)", 
+      "e-testAnalysis": "평가A분석", 
+      "e-testSupplement": "보완학습(과제와 테스트로 추가개념과 문제)", 
+      "e-examSimulation": "내신모의평가", 
+      "e-examSimulationAnalysis": "내신모의평가분석", 
+      "e-examSimulationSupplement": "내신대비보완학습", 
+      "e-satSimulation": "수능모의평가", 
+      "e-satSimulationAnalysis": "수능모의평가분석", 
+      "e-satSimulationSupplement": "수능대비보완학습"
+    },
+    "진도와분석(progress)": {
+      "p-learningCondition": "진도확인(학습진행상황)", 
+      "p-learningAnalysis": "단원별성취도분석(나의학습분석)", 
+      "p-learningAssistant": "학습방향조언(학습진행도우미)"
+    }
+  };
+
